23d37a61ea1a7e29feea7a8bc17a28360660d993
Jay Ser <jayser6@gmail.com>
2022-12-04 12:18:26 -0800

    stuff

--- xcamp/11.12.22/chip/chip.cpp
+++ xcamp/11.12.22/chip/chip.cpp
@@ -1,15 +1,15 @@
 /*____________________________________________________________
-// started : 11/26/22
+// started : 12/04/22
 // finished:
 // problem : https://codeforces.com/problemset/problem/1716/D
 ____________________________________________________________*/
 
 #include <bits/stdc++.h>
 
-#define FOR(n) for (int i = 0;i < n;i++)
-#define FORO(n) for (int i = 1;i < n;i++)
-#define ROF(n) for (int i = n - 1;i >= 0;i--)
-#define ROFO(n) for (int i = n - 1;i >= 1;i--)
+#define FOR(i, n) for (int i = 0;i < n;i++)
+#define FORO(i, n) for (int i = 1;i < n;i++)
+#define ROF(i, n) for (int i = n - 1;i >= 0;i--)
+#define ROFO(i, n) for (int i = n - 1;i >= 1;i--)
 #define loop while (true) // rust woooo
 #define ALL(arr) arr.begin(), arr.end()
 #define lower lower_bound 
@@ -42,6 +42,40 @@ int main() {
     cin.tie(0);
 
     int n, k; cin >> n >> k;
+
+    vector<ll> ans(n + 1, 0);
+
+    /* dp ans solve */
+
+    vector<vector<ll>> dp(2, vector<ll>(n + 1, 0));
+    bool finished = false;
+    int steps = 0;
+
+    dp[0][0] = 1;
+
+    while (!finished) {
+        steps++;
+        finished = true;
+
+        FOR(j, n + 1) {
+            int pull_index = j - (k + steps - 1);
+
+            if (pull_index >= 0) {
+                dp[steps % 2][j] = dp[!(steps % 2)][pull_index] + dp[steps % 2][pull_index];
+                dp[steps % 2][j] %= MOD;
+                ans[j] += dp[steps % 2][j];
+                ans[j] %= MOD;
+
+                if (dp[steps % 2][j] > 0) {
+                    finished = false;
+                }
+            }
+        }
+    }
+
+    FORO(i, n + 1) {
+        cout << ans[i] << " ";
+    }
 
     return 0;
 }
