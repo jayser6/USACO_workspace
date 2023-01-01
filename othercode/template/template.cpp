4f31375b7fcdac3f60dcb11aff91eef635c73801
Jay Ser <jayser6@gmail.com>
2023-01-01 14:01:46 -0800

    template

--- othercode/template/template.cpp
+++ othercode/template/template.cpp
@@ -10,6 +10,7 @@ using namespace std;
 #pragma GCC optimize ("Ofast")
 #pragma GCC target ("avx2")
 
+#define debug(x) #x << ": " << x << " "
 #define FOR(i, n) for (int i = 0;i < n;i++)
 #define FORO(i, n) for (int i = 1;i < n;i++)
 #define ROF(i, n) for (int i = n - 1;i >= 0;i--)
