88b75e75c0b71a40acaaf347b3e280e4fbb52484
Jay Ser <jayser6@gmail.com>
2023-01-05 12:32:41 -0800

    solved fighting tournament

--- xcamp/11.05.22/fighting/fighting.cpp
+++ xcamp/11.05.22/fighting/fighting.cpp
@@ -1,21 +1,27 @@
 /*____________________________________________________________
-// started : 11/08/22
-// finished:
+// started : 01/05/23
+// finished: 01/05/23
 // problem : https://codeforces.com/problemset/problem/1719/C
 ____________________________________________________________*/
 
 #include <bits/stdc++.h>
+using namespace std;
 
-#define FOR(n) for (int i = 0;i < n;i++)
-#define FORO(n) for (int i = 1;i < n;i++)
-#define ROF(n) for (int i = n - 1;i >= 0;i--)
-#define ROFO(n) for (int i = n - 1;i >= 1;i--)
+// #pragma GCC optimize ("Ofast")
+// #pragma GCC target ("avx2")
+
+#define debug(x) #x << ": " << x << " "
+#define FOR(i, n) for (int i = 0;i < n;i++)
+#define FORO(i, n) for (int i = 1;i < n;i++)
+#define ROF(i, n) for (int i = n - 1;i >= 0;i--)
+#define ROFO(i, n) for (int i = n - 1;i >= 1;i--)
 #define loop while (true) // rust woooo
 #define ALL(arr) arr.begin(), arr.end()
 #define lower lower_bound 
 #define upper upper_bound
 #define ll long long
 #define uint unsigned int
+#define tup tuple
 #define hashmap unordered_map
 #define hashset unordered_set
 #define p_queue priority_queue
@@ -27,55 +33,44 @@ ____________________________________________________________*/
 #define f first
 #define s second
 #define endl "\n"
-#define BIG_NUMBER (ll)1e18
-#define BIG_PRIME (ll)999998727899999 // this number has 15 digits
+#define BIG (int)INT_MAX 
+#define BIGGER (ll)LLONG_MAX
+#define PRIME64 (ll)999998727899999 // this number has 15 digits
 #define PRIME32 (int)(1e9 + 7) 
 #define ASCII_PRIME (int)257
 #define ALPHA_PRIME (int)29
 
-using namespace std;
+const int MAX_N = 1e5;
+int a[MAX_N];
+pair<int, int> r[MAX_N]; // { first win, last win }, both inclusive
 
 void solve() {
     int n, q; cin >> n >> q;
 
-    vector<int> fighters(n);
-
-    FOR(n)
-        cin >> fighters[i];
-
-    vector<int> simul(n * 2);
-
-
-    vector<int> win_cnt(n, 0);
-    int index = 0;
-
-    while (simul[index] != n) {
-        if (simul[index] > simul[index + 1]) {
-            // same winner
-
-            simul[index + n] = simul[index + 1];
-            simul[index + 1] = simul[index];
-        }
-        else {
-            // new winner
-
-            simul[index + n] = simul[index];
-        }
-
-        index++;
+    FOR(i, n) {
+        cin >> a[i];
+        r[i] = { -1, -1 };
     }
 
-    FOR(q) {
-        int fi, k; cin >> fi >> k;
+    int front = 0;
+    r[front] = { 1, -1 };
 
-        if (fi == n) {
-            cout << k - index;
+    FORO(i, n) {
+        if (a[i] > a[front]) {
+            front = i;
+            r[front] = { i, i };
         }
-        else {
-
-        }
+        else r[front].s = i;
     }
 
+    FOR(q_i, q) {
+        int i, k; cin >> i >> k;
+        i--;
+
+        if (r[i].s == -1 || r[i].f > k) cout << 0 << endl;
+        else if (a[i] == n) cout << k - r[i].f + 1 << endl;
+        else cout << min(r[i].s, k) - r[i].f + 1 << endl;
+    }
 }
 
 int main() {
@@ -83,8 +78,7 @@ int main() {
     cin.tie(0);
 
     int t; cin >> t;
-    while (t--)
-        solve();
+    while (t--) solve();
 
     return 0;
 }
