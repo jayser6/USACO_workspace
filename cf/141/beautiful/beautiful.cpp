29ae895e30062246fa8305f6d7a211187277fc43
Jay Ser <jayser6@gmail.com>
2023-01-08 08:42:25 -0800

    wow 2 problems in a cf contest haha

---
+++ cf/141/beautiful/beautiful.cpp
@@ -0,0 +1,74 @@
+/*____________________________________________________________
+// started : 01/08/23
+// finished: 01/08/23
+// problem : https://codeforces.com/contest/1783/problem/0
+____________________________________________________________*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// #pragma GCC optimize ("Ofast")
+// #pragma GCC target ("avx2")
+
+#define debug(x) #x << ": " << x << " "
+#define FOR(i, n) for (int i = 0;i < n;i++)
+#define FORO(i, n) for (int i = 1;i < n;i++)
+#define ROF(i, n) for (int i = n - 1;i >= 0;i--)
+#define ROFO(i, n) for (int i = n - 1;i >= 1;i--)
+#define loop while (true) // rust woooo
+#define ALL(arr) arr.begin(), arr.end()
+#define lower lower_bound 
+#define upper upper_bound
+#define ll long long
+#define uint unsigned int
+#define tup tuple
+#define hashmap unordered_map
+#define hashset unordered_set
+#define p_queue priority_queue
+#define pb push_back
+#define pf push_front
+#define popb pop_back
+#define popf pop_front
+#define mp make_pair    
+#define f first
+#define s second
+#define endl "\n"
+#define BIG (int)INT_MAX 
+#define BIGGER (ll)LLONG_MAX
+#define SMALL (int)INT_MIN
+#define SMALLER (ll)LLONG_MIN
+#define PRIME64 (ll)999998727899999 // this number has 15 digits
+#define PRIME32 (int)(1e9 + 7) 
+#define ASCII_PRIME (int)257
+#define ALPHA_PRIME (int)29
+
+void solve() {
+    int n; cin >> n;
+    vector<int> arr(n);
+    FOR(i, n) cin >> arr[i];
+    sort(ALL(arr));
+
+    int sp = 0, ep = n - 1;
+    if (arr[sp] == arr[ep]) {
+        cout << "NO" << endl;
+        return;
+    }
+    cout << "YES" << endl;
+    while (sp < ep) {
+        cout << arr[ep] << " " << arr[sp] << " ";
+        sp++, ep--;
+    }
+    if (sp == ep) cout << arr[sp];
+    cout << endl;
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t; cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
---
+++ cf/141/tournament/tournament.cpp
@@ -0,0 +1,52 @@
+/*____________________________________________________________
+// started : 01/08/23
+// finished:
+// problem : https://codeforces.com/contest/1783/problem/C
+____________________________________________________________*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// #pragma GCC optimize ("Ofast")
+// #pragma GCC target ("avx2")
+
+#define debug(x) #x << ": " << x << " "
+#define FOR(i, n) for (int i = 0;i < n;i++)
+#define FORO(i, n) for (int i = 1;i < n;i++)
+#define ROF(i, n) for (int i = n - 1;i >= 0;i--)
+#define ROFO(i, n) for (int i = n - 1;i >= 1;i--)
+#define loop while (true) // rust woooo
+#define ALL(arr) arr.begin(), arr.end()
+#define lower lower_bound 
+#define upper upper_bound
+#define ll long long
+#define uint unsigned int
+#define tup tuple
+#define hashmap unordered_map
+#define hashset unordered_set
+#define p_queue priority_queue
+#define pb push_back
+#define pf push_front
+#define popb pop_back
+#define popf pop_front
+#define mp make_pair    
+#define f first
+#define s second
+#define endl "\n"
+#define BIG (int)INT_MAX 
+#define BIGGER (ll)LLONG_MAX
+#define SMALL (int)INT_MIN
+#define SMALLER (ll)LLONG_MIN
+#define PRIME64 (ll)999998727899999 // this number has 15 digits
+#define PRIME32 (int)(1e9 + 7) 
+#define ASCII_PRIME (int)257
+#define ALPHA_PRIME (int)29
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+
+
+    return 0;
+}
